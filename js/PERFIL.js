1318ea8b6a825f1375dfde932fafbf8fb41caa61
marcoscaloggi <marcoscaloggi@gmail.com>
2019-11-01 06:55:15 -0300

    asd

---
+++ js/PERFIL.js
@@ -0,0 +1,10 @@
+// function inicioPag(){
+//
+// function actualizar(){
+// location.reload(true);
+// }
+// //Función para actualizar cada 4 segundos(4000 milisegundos)
+// setInterval("actualizar()",4000);
+// }
+//
+// document.addEventListener("DOMContentLoaded",inicioPag);
